a44c90d9747d7c6cac40ff75b0a632e4a4f88ebd
Aliyah ALrasheedi <koonay6@gmail.com>
2026-03-31 20:36:08 +0300

    Force adding all project files

---
+++ djangoenv/DjangoProjects/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import include, path
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('books/', include("apps.bookmodule.urls")),
+    path('users/', include("apps.usermodule.urls")),
+]
